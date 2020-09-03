51a894630c0da0429be2c8a14fe69df1d5886781
Alden <aldensmith06@Gmail.com>
2020-09-03 16:42:45 -0700

    Chaned gravity physics to be used by fixed update

--- Assets/Gravity.cs
+++ Assets/Gravity.cs
@@ -8,7 +8,7 @@ public class Gravity : MonoBehaviour
     
     [SerializeField] GameObject _world;
     
-    [SerializeField] float _gravityScale = 1;
+    [SerializeField] float _gravityScale = 9.81f;
 
     Rigidbody2D rb;
 
@@ -17,10 +17,9 @@ public class Gravity : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
     }
 
-    private void Update()
+    private void FixedUpdate() //fixedupdate should be used for physics
     {
         float distance = Vector2.Distance(_world.transform.position, this.transform.position);
-
         Vector3 direction = _world.transform.position - transform.position;
         rb.AddForce(direction.normalized * _gravityScale);
     }
