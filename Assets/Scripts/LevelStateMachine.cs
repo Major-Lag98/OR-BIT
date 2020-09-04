4f79dfc47a238c99155cdec2c4f0afde25a39ab3
Alden <aldensmith06@Gmail.com>
2020-09-03 18:25:29 -0700

    Created Level State Machine

---
+++ Assets/Scripts/LevelStateMachine.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelStateMachine : MonoBehaviour
+{
+
+    public enum State 
+    {
+        Intro,      //Let things smoothly settle in after scene load
+        Idle,       //Is when you are waiting for input from the player to launch the object
+        Playing,    //Is when you have launched an object and waiting for it to settle
+        Pause,
+        Lose,
+        win
+    }
+
+    public State state;
+
+    public static LevelStateMachine Instance; //make singleton
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+    }
+
+}
