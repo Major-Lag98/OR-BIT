395ac7b0238d7b1177b19742003cbf4e0257f9cb
Alden <aldensmith06@Gmail.com>
2020-09-05 16:40:31 -0700

    Limit edge collider is now more forgiving

--- Assets/Scripts/PieceFlinger.cs
+++ Assets/Scripts/PieceFlinger.cs
@@ -28,7 +28,7 @@ public class PieceFlinger : MonoBehaviour
     bool isFlinging = false;
 
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
         if (Instance == null)
             Instance = this;
