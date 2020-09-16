f7ec62e6957dd09a08dea569bbc6ac3e1834370c
Alden <aldensmith06@Gmail.com>
2020-09-16 00:06:54 -0700

    Small code cleanup

--- Assets/Scripts/LevelStateMachine.cs
+++ Assets/Scripts/LevelStateMachine.cs
@@ -1,17 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using TMPro;
 using UnityEngine.SceneManagement;
 
 public class LevelStateMachine : MonoBehaviour
 {
 
-    [SerializeField] GameObject Piece = null;
-
     [SerializeField] Transform spawnPosition;
-
-    //[SerializeField] TextMeshProUGUI nextText;
 
     Animator uiAnimator;
 
