ddc0221e0e2f7d45bcf824125c33794518567b00
Alden <aldensmith06@Gmail.com>
2020-09-19 12:08:04 -0700

    Animation for MainMenu
    Mostlikely going to switch to tweens

--- Assets/Scripts/LevelSelect.cs
+++ Assets/Scripts/LevelSelect.cs
@@ -5,8 +5,24 @@ using UnityEngine.SceneManagement;
 
 public class LevelSelect : MonoBehaviour
 {
+
+    [SerializeField] Animator mainMenuAnimations;
+
     public void LoadScene(int buildIndex)
     {
         SceneManager.LoadScene(buildIndex);
     }
+
+    public void LevelSelectFocus()
+    {
+        mainMenuAnimations.SetTrigger("LevelSelectFocus"); //needs implementation
+    }
+
+    public void MainMenuFocus()
+    {
+        mainMenuAnimations.SetTrigger("MainMenuFocus"); //needs implementation
+    }
+
+
+
 }
